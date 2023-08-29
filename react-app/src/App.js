1aa7a84922b4125d939419726ae1077031b9941d
edvni <edvard.nivala@metropolia.fi>
2023-08-29 12:44:11 +0300

    App.js and login.js
    Added the simplest login/signup page for initial testing purposes. Will be developed heavily still...

---
+++ react-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import LoginSignup from './login.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginSignup />
+    </div>
+  );
+}
+
+export default App;
