100b6a024f057646a11ec253c1b24c1bcaf1dab1
edvni <edvard.nivala@metropolia.fi>
2023-09-02 17:45:35 +0300

    Login page CSS

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { useAuthState } from "react-firebase-hooks/auth"
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import { auth } from "./firebaseConfig"; // Import Firebase config
+import wfLogo from './images/WhisperFeed_Logo.png';
+
 
 import SignIn from './components/Auth/SignIn';
 import SignOut from './components/Auth/SignOut';
@@ -14,16 +16,24 @@ function App() {
   const [user] = useAuthState(auth);
 
   return (
-      <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
-        <header className="bg-blue-500 text-white p-4">
-          <h1 className="text-3xl font-bold underline">WhisperFeed</h1>
-          <p className="text-lg">{user ? <p>Welcome {user.displayName}!</p> : <p className="text-lg">Please log in!</p>}</p>
-          <SignOut/>
-      </header>
-      <section className="py-8">
-        {/* Only open FeedbackMain if user is signed in and has a valid uid, else sign in */}
-        {user ? <FeedbackMain/>  : <SignIn/>}
-      </section>
+      <div class="flex h-screen w-full items-center justify-center bg-gray-900 bg-cover bg-no-repeat">
+          <div class="rounded-xl bg-gray-800 bg-opacity-50 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8">
+              <div class="text-white">
+                  <div class="mb-8 flex flex-col items-center">
+                      <img src={wfLogo} alt="logo" class="w-2/3 h-auto"/>
+                      <header>
+                          <p>{user ? <p>Welcome {user.displayName}!</p> : <p className="text-lg">Please log in!</p>}</p>
+                          <SignOut/>
+                      </header>
+                      <section className="py-8">
+                          {/* Only open FeedbackMain if user is signed in and has a valid uid, else sign in */}
+                          {user ? <FeedbackMain/>  : <SignIn/>}
+                      </section>
+                  </div>
+
+              </div>
+
+        </div>
     </div>
   );
 }
