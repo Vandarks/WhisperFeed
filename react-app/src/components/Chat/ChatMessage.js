cd96346f5903536072e287d5ca871db858539295
Higic <onni.alasaari@gmail.com>
2023-09-02 15:25:00 +0300

    Added Course creation and course removal

--- react-app/src/components/Chat/ChatMessage.js
+++ react-app/src/components/Chat/ChatMessage.js
@@ -1,9 +1,32 @@
 import React from "react";
+import { auth, firestore } from '../../firebaseConfig'; // Import firestore from your Firebase configuration file
+import { doc, deleteDoc } from "firebase/firestore";
 
 function ChatMessage(props) {
-    const {text, uid} = props.message;
+    const {text, uid, photoURL, displayName, generatedDocId} = props.message;
 
-    return <p>{text}</p>
+    const docId = props.id;
+
+    const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
+
+    const removeCourse = async(e) => {
+        console.log(generatedDocId);
+        console.log(docId);
+        const docRef = doc(firestore, "messages", docId);
+        deleteDoc(docRef);
+        
+    }
+ 
+
+    return (
+        <div>
+            <img src={photoURL} alt="Creator"/>
+            <p>{text}, Created by: {displayName}</p>
+            { messageClass ==="sent" && (
+                <button onClick={removeCourse}>Remove course</button>
+            )}
+        </div>
+    )
 }
 
 export default ChatMessage;
--- react-app/src/components/Feedback/FeedbackMain.js
+++ react-app/src/components/Feedback/FeedbackMain.js
@@ -1,7 +1,10 @@
 import {useCollectionData} from "react-firebase-hooks/firestore";
-import React from "react";
-import { firestore } from '../../firebaseConfig'; // Import firestore from your Firebase configuration file
+import React, { useState } from "react";
+import { firestore, auth } from '../../firebaseConfig'; // Import firestore from your Firebase configuration file
 import ChatMessage from '../Chat/ChatMessage';
+import firebase from 'firebase/compat/app';
+import { setDoc } from "firebase/firestore";
+
 
 // Main feedback component of the app
 // TODO: Put two different views, one for teachers and one for students
@@ -11,15 +14,47 @@ function FeedbackMain() {
     const messagesRef = firestore.collection("messages");
     const query = messagesRef.orderBy("createdAt").limit(25);
 
+    const [docId, setDocId] = useState("");
+
     const [messages] = useCollectionData(query, {idField: "id"});
+
+    const [formValue, setFormValue] = useState("");
+
+    const createCourse = async(e) => {
+        e.preventDefault();
+
+        const { uid, photoURL, displayName } = auth.currentUser;
+
+        await messagesRef.add({
+            text: formValue,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            uid,
+            photoURL,
+            displayName
+        })
+        .then((docRef) => {
+            console.log("Document ID: ", docRef.id);
+            setDocId(docRef.id);
+        })
+        .catch((e) => {
+            console.error("Error adding document: ", e);
+        });
+
+        setFormValue("");
+    }
+
+
+    
 
     return (
         <>
-            <div>
                 <div>
-                    {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+                    {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} id={docId}/>)}
                 </div>
-            </div>
+                <form onSubmit={createCourse}>
+                    <input value={formValue} onChange={(e) => setFormValue(e.target.value)}/>
+                    <button type="submit">Create course</button>
+                </form>
         </>
     )
 }
