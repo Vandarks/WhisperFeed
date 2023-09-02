5623ce0750c1be62a2277f57dd1bd5f8ebf12a7a
Higic <onni.alasaari@gmail.com>
2023-09-02 23:17:12 +0300

    Course deletion

--- react-app/src/components/Chat/ChatMessage.js
+++ react-app/src/components/Chat/ChatMessage.js
@@ -1,29 +1,41 @@
 import React from "react";
 import { auth, firestore } from '../../firebaseConfig'; // Import firestore from your Firebase configuration file
-import { doc, deleteDoc } from "firebase/firestore";
 
 function ChatMessage(props) {
-    const {text, uid, photoURL, displayName, generatedDocId} = props.message;
+    const {text, uid, photoURL, displayName } = props.message;
 
-    const docId = props.id;
 
     const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
 
-    const removeCourse = async(e) => {
-        console.log(generatedDocId);
-        console.log(docId);
-        const docRef = doc(firestore, "messages", docId);
-        deleteDoc(docRef);
-        
+
+    // Remove course from database
+    const handleButtonClick = () => {
+
+        const courseName = props.message.text;
+        const courseCreator = props.message.uid;
+
+        console.log("course: ", courseName, " uid: ", courseCreator)
+
+        firestore.collection("messages")
+        .where("text", "==", courseName)
+        .get()
+        .then(querySnapshot => {
+            querySnapshot.docs[0].ref.delete();
+        });
     }
- 
+
+    // template for feedback that shows only for the creator, currently hardcoded
+    const feedback = "olipa tyhäm kursi >:D";
 
     return (
         <div>
             <img src={photoURL} alt="Creator"/>
             <p>{text}, Created by: {displayName}</p>
             { messageClass ==="sent" && (
-                <button onClick={removeCourse}>Remove course</button>
+                <div>
+                <p>Feedback: {feedback} </p>
+                <button onClick={handleButtonClick}>Remove course</button>
+                </div>
             )}
         </div>
     )
