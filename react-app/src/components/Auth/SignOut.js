dc3578789c5cbb7731bb9de6d7031d5a23ddcf78
edvni <edvard.nivala@metropolia.fi>
2023-09-24 17:21:50 +0300

    Update SignOut.js
    Button changes and stuff

--- react-app/src/components/Auth/SignOut.js
+++ react-app/src/components/Auth/SignOut.js
@@ -6,8 +6,8 @@ import React from "react";
 function SignOut() {
     return auth.currentUser && (
 
-        <div className="mt-8 flex justify-center text-lg text-black">
-            <button onClick={() => auth.signOut()} class="rounded-3xl bg-orange-500 bg-opacity-50 px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-yellow-600">Sign out</button>
+        <div className="">
+            <button onClick={() => auth.signOut()} class="rounded-3xl bg-orange-500 bg-opacity-50 px-4 py-1.5 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-yellow-600">Sign out</button>
         </div>
     )
 }
