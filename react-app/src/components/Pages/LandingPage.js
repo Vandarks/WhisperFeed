2b18e7207864b4fd696b419421185c5b29c992de
edvni <edvard.nivala@metropolia.fi>
2023-09-24 17:23:01 +0300

    Update LandingPage.js
    if user is in, disable logo and "please log in" text

--- react-app/src/components/Pages/LandingPage.js
+++ react-app/src/components/Pages/LandingPage.js
@@ -1,7 +1,6 @@
 import {useAuthState} from "react-firebase-hooks/auth";
 import {auth} from "../../firebaseConfig";
 import wfLogo from "../../images/WhisperFeed_Logo.png";
-import SignOut from "../Auth/SignOut";
 import CoursesMain from "../Courses/CoursesMain";
 import SignIn from "../Auth/SignIn";
 import React from "react";
@@ -10,15 +9,12 @@ function LandingPage() {
     const [user] = useAuthState(auth);
 
     return (
-        <div className="flex min-h-screen w-full items-center justify-center bg-gray-900 bg-cover bg-no-repeat">
-            <div className="my-10 rounded-xl bg-gray-800 bg-opacity-50 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8">
-                <div className="text-white">
-                    <div className="mb-8 flex flex-col items-center">
-                        <img src={wfLogo} alt="logo" className="w-1/3"/>
-                        <header>
-                            <p>{user ? <p>Welcome {user.displayName}!</p> : <p className="text-lg">Please log in!</p>}</p>
-                            <SignOut/>
-                        </header>
+        <div>
+            <div className="flex min-h-screen w-full items-center justify-center bg-gray-900 bg-cover bg-no-repeat">
+                <div className="my-10 rounded-xl bg-gray-800 bg-opacity-50 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8">
+                    <div className="mb-8 flex flex-col items-center text-white">
+                        {user ? null : <img src={wfLogo} alt="logo" className="w-1/3"/>}
+                        <p>{user ? null : <p className="text-lg">Please log in!</p>}</p>
                         <section className="py-8">
                             {/* Only open CoursesMain if user is signed in and has a valid uid, else sign in */}
                             {user ? <CoursesMain className="grid-cols-4 gap-4"/>  : <SignIn/>}
