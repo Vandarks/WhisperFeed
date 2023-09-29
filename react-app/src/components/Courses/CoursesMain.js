b65f0c044b0b4d931d7ff1903e72c6e4f4f9b89a
Vandarks <majewski.miiko@gmail.com>
2023-09-29 03:53:42 +0300

    Added check for event type in course creation and course type is now sent to the database

--- react-app/src/components/Courses/CoursesMain.js
+++ react-app/src/components/Courses/CoursesMain.js
@@ -6,7 +6,6 @@ import firebase from 'firebase/compat/app';
 import Modal from 'react-modal';
 
 // Main feedback component of the app
-// TODO: Put two different views, one for teachers and one for students
 // Students should be able to comment or give a rating to the course created by the teacher.
 // The creator of the course should see the feedback on the course, maybe as a 0-5 star system?
 function CoursesMain() {
@@ -17,7 +16,9 @@ function CoursesMain() {
 
     const [courses] = useCollectionData(query, {idField: "id"});
 
-    const [formValue, setFormValue] = useState("");
+    const [formCourseName, setFormCourseName] = useState("");
+
+    const [formCourseType, setFormCourseType] = useState("");
 
     const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -34,12 +35,13 @@ function CoursesMain() {
         e.preventDefault();
 
         // Only when course name > 5 characters
-        if (formValue.length > 5) {
+        if (formCourseName.length > 5 && formCourseType != "") {
 
         const { uid, photoURL, displayName } = auth.currentUser;
 
         await coursesRef.add({
-            courseName: formValue,
+            courseName: formCourseName,
+            courseType: formCourseType,
             createdAt: firebase.firestore.FieldValue.serverTimestamp(),
             uid,
             photoURL,
@@ -53,7 +55,7 @@ function CoursesMain() {
             console.error("Error adding document: ", e);
         });
 
-        setFormValue("");
+        setFormCourseName("");
         } else {
             console.log("Course not created, add valid course name!")
         }
@@ -69,14 +71,16 @@ function CoursesMain() {
                     isOpen={isModalOpen}
                     onRequestClose={closeModal}
                     createCourse={createCourse}
-                    formValue={formValue}
-                    setFormValue={setFormValue}
+                    formCourseName={formCourseName}
+                    setFormCourseName={setFormCourseName}
+                    formCourseType={formCourseType}
+                    setFormCourseType={setFormCourseType}
                 />
                 <form onSubmit={createCourse} className="mb-5">
                     <p>New Event</p>
-                    <input value={formValue}
+                    <input value={formCourseName}
                            placeholder="Enter a name, min. 5 characters"
-                           onChange={(e) => setFormValue(e.target.value)}
+                           onChange={(e) => setFormCourseName(e.target.value)}
                            className="w-3/12 min-w-min mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
                     <button type="submit" className="bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create course</button>
                 </form>
@@ -90,7 +94,7 @@ function CoursesMain() {
     )
 }
 
-function CourseModal({ isOpen, onRequestClose, createCourse, formValue, setFormValue }) {
+function CourseModal({ isOpen, onRequestClose, createCourse, formCourseName, setFormCourseName, formCourseType, setFormCourseType}) {
 
     return (
         <Modal
@@ -121,21 +125,23 @@ function CourseModal({ isOpen, onRequestClose, createCourse, formValue, setFormV
                         {/* Right Section */}
                         <div className="col-span-1 mr-5">
                             <input
-                                value={formValue}
+                                value={formCourseName}
                                 placeholder="Enter a name, min. 5 characters"
-                                onChange={(e) => setFormValue(e.target.value)}
+                                onChange={(e) => setFormCourseName(e.target.value)}
                                 className="w-full mb-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             />
                             <label htmlFor="event-types" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                                 Select an option
                             </label>
                             <select
+                                value={formCourseType}
+                                onChange={(e) => setFormCourseType(e.target.value)}
                                 id="event-types"
                                 className="mb-5 w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             >
                                 <option selected>Choose event type</option>
-                                <option value="US">Course</option>
-                                <option value="CA">Event</option>
+                                <option value="Course">Course</option>
+                                <option value="Event">Event</option>
                             </select>
                             <button
                                 type="submit"
