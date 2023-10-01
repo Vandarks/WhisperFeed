c6e3c3255338ab73dd7eac3e256400feeb5445bf
edvni <edvard.nivala@metropolia.fi>
2023-10-01 23:12:43 +0300

    refactoring + burger button stuff
    burger button work in progress

--- react-app/src/components/Layout/HeaderProfile.js
+++ react-app/src/components/Layout/HeaderProfile.js
@@ -10,7 +10,13 @@ import React, {useState} from "react";
 function HeaderProfile() {
     const [user] = useAuthState(auth);
 
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
     const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
 
     const openModal = () => {
         setIsModalOpen(true);
@@ -50,15 +56,15 @@ function HeaderProfile() {
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
                                  xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
+                                <path fillRule="evenodd"
                                       d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                                      clip-rule="evenodd"></path>
+                                      clipRule="evenodd"></path>
                             </svg>
                             <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
                                  xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
+                                <path fillRule="evenodd"
                                       d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                      clip-rule="evenodd"></path>
+                                      clipRule="evenodd"></path>
                             </svg>
                         </button>
                     </div>
@@ -112,8 +118,8 @@ function SettingsModal({ isOpen, onRequestClose }) {
                                 data-modal-hide="authentication-modal">
                             <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                                  viewBox="0 0 14 14">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                      stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                                      strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                             </svg>
                             <span className="sr-only">Close modal</span>
                         </button>
@@ -158,7 +164,7 @@ function SettingsModal({ isOpen, onRequestClose }) {
             </div>
         </Modal>
     );
-};
+}
 
 
 export default HeaderProfile
