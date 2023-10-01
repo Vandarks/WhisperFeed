2b03f85eb83dd1779ef30397870e3883032b5e3b
Vandarks <majewski.miiko@gmail.com>
2023-10-02 00:19:48 +0300

    Added signup logic to SignUpForm.js

--- react-app/src/components/Auth/SignUpForm.js
+++ react-app/src/components/Auth/SignUpForm.js
@@ -1,4 +1,52 @@
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {useState} from "react";
+
 function SignUpForm({ onSignInClick }) {
+
+    //Code for creating a new account in firebase
+    const auth = getAuth();
+    const signUp = (e) => {
+        e.preventDefault();
+        if(password.length >= 6 && email != "" && firstName != "" && lastName != ""){
+            createUserWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                    // Signed up
+                    //const user = userCredential.user;
+                    // ...
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    console.log(errorCode + " " + errorMessage)
+                });
+        }else {
+            if(password.length < 6){
+                console.log("Password should be at least 6 characters long");
+            }else{
+                console.log("Please fill in all fields");
+            }
+        }
+    };
+
+    //Code for handling form input values
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+    const handleFirstNameChange = (e) => {
+        setFirstName(e.target.value);
+    };
+    const handleLastNameChange = (e) => {
+        setLastName(e.target.value);
+    };
+
 
     return (
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
@@ -6,28 +54,28 @@ function SignUpForm({ onSignInClick }) {
                 <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                     Sign up with a new account
                 </h1>
-                <form className="space-y-4 md:space-y-6" action="#">
+                <form onSubmit={signUp} className="space-y-4 md:space-y-6" action="#">
                     <div>
                         <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                        <input type="email" name="email" id="email"
+                        <input type="email" name="email" id="email" value={email} onChange={handleEmailChange}
                                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                placeholder="name@company.com" required=""/>
                     </div>
                     <div>
                         <label htmlFor="fname" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
-                        <input type="text" name="fname" id="fname"
+                        <input type="text" name="fname" id="fname" value={firstName} onChange={handleFirstNameChange}
                                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                placeholder="First name" required=""/>
                     </div>
                     <div>
                         <label htmlFor="lname" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last name</label>
-                        <input type="text" name="lname" id="lname"
+                        <input type="text" name="lname" id="lname" value={lastName} onChange={handleLastNameChange}
                                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                placeholder="Last name" required=""/>
                     </div>
                     <div>
                         <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                        <input type="password" name="password" id="password"
+                        <input type="password" name="password" id="password" value={password} onChange={handlePasswordChange}
                                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                placeholder="••••••••" required=""/>
                     </div>
