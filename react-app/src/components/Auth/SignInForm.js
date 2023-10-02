c1b32a01b3d616215b419d12293f2120c0e06d10
edvni <edvard.nivala@metropolia.fi>
2023-10-02 10:36:28 +0300

    SignInForm + alert update

--- react-app/src/components/Auth/SignInForm.js
+++ react-app/src/components/Auth/SignInForm.js
@@ -51,17 +51,6 @@ function SignInForm({ onSignUpClick }) {
                                required=""/>
                     </div>
                     <div className="flex items-center justify-between">
-                        <div className="flex items-start">
-                            <div className="flex items-center h-5">
-                                <input id="remember" aria-describedby="remember" type="checkbox"
-                                       className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                                       required="" />
-                            </div>
-                            <div className="ml-3 text-sm">
-                                <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember
-                                    me</label>
-                            </div>
-                        </div>
                         <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                     </div>
                     <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
