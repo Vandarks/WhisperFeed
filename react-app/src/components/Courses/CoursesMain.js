6e78d9f12bc8db3a5cf9c316675bcdb6502685ca
Miiko Majewski <miiko.majewski@metropolia.fi>
2023-10-31 14:22:43 +0200

    Used prettier to format code in CoursesMain

--- react-app/src/components/Courses/CoursesMain.js
+++ react-app/src/components/Courses/CoursesMain.js
@@ -1,15 +1,14 @@
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import React, { useEffect, useState } from "react";
-import { coursesRef, auth, usersRef } from '../../firebaseConfig'; // Import firestore from your Firebase configuration file
-import Course from '../Chat/Course';
-import firebase from 'firebase/compat/app';
-import Modal from 'react-modal';
+import { coursesRef, auth, usersRef } from "../../firebaseConfig"; // Import firestore from your Firebase configuration file
+import Course from "../Chat/Course";
+import firebase from "firebase/compat/app";
+import Modal from "react-modal";
 
 // Main feedback component of the app
 // Students should be able to comment or give a rating to the course created by the teacher.
 // The creator of the course should see the feedback on the course, maybe as a 0-5 star system?
 function CoursesMain() {
-
     // All known course keys
     const [knownKeys, setKnownKeys] = useState([""]);
 
@@ -21,7 +20,6 @@ function CoursesMain() {
 
     const [courseKeyText, setCourseKeyText] = useState("");
 
-
     const [courses] = useCollectionData(codeQuery, { idField: "id" });
 
     const [formCourseName, setFormCourseName] = useState("");
@@ -30,9 +28,9 @@ function CoursesMain() {
 
     const [isModalOpen, setIsModalOpen] = useState(false);
 
-
     const refreshCourses = () => {
-        currentUserRef.get()
+        currentUserRef
+            .get()
             .then((doc) => {
                 if (doc.exists) {
                     const userData = doc.data();
@@ -43,12 +41,15 @@ function CoursesMain() {
                         console.log("Field does not exist in the doc");
                     }
                 } else {
-                    console.log("Document does not exist in collection, creating");
-                    currentUserRef.set({
-                        courseCodes: ["vZVV66"],
-                    })
+                    console.log(
+                        "Document does not exist in collection, creating"
+                    );
+                    currentUserRef
+                        .set({
+                            courseCodes: ["vZVV66"],
+                        })
                         .then(() => {
-                            console.log("New document created succesfully.")
+                            console.log("New document created succesfully.");
                         })
                         .catch((e) => {
                             console.error("Error creating a new document: ", e);
@@ -63,37 +64,45 @@ function CoursesMain() {
             .get()
             .then((querySnapshot) => {
                 querySnapshot.forEach((doc) => {
-                    console.log("Doc id: " + doc.id + " course name: " + doc.data().courseName);
+                    console.log(
+                        "Doc id: " +
+                            doc.id +
+                            " course name: " +
+                            doc.data().courseName
+                    );
                 });
-            }).catch((error) => {
+            })
+            .catch((error) => {
                 console.error("Error getting user documents: ", error);
             });
-    }
+    };
 
     let generatedKey = "";
     // For generating a unique key for course
     const generateCourseKey = () => {
-        const characters = "1234567890qwertyuiopsdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZCVNM";
+        const characters =
+            "1234567890qwertyuiopsdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZCVNM";
         const keyLength = 6;
         let counter = 0;
         let uniqueKeyGenerated = false;
 
         //Retrieving all known keys from document
-        coursesRef.get()
-            .then((querySnapshot) => {
-                const keys = [];
-                querySnapshot.forEach((doc) => {
-                    const data = doc.data();
-                    if (data.hasOwnProperty("courseKey")) {
-                        keys.push(data.courseKey);
-                    }
-                })
-                setKnownKeys(keys);
+        coursesRef.get().then((querySnapshot) => {
+            const keys = [];
+            querySnapshot.forEach((doc) => {
+                const data = doc.data();
+                if (data.hasOwnProperty("courseKey")) {
+                    keys.push(data.courseKey);
+                }
             });
+            setKnownKeys(keys);
+        });
 
         while (!uniqueKeyGenerated) {
             while (counter < keyLength) {
-                generatedKey += characters.charAt(Math.floor(Math.random() * characters.length));
+                generatedKey += characters.charAt(
+                    Math.floor(Math.random() * characters.length)
+                );
                 counter += 1;
             }
             counter = 0;
@@ -101,21 +110,19 @@ function CoursesMain() {
                 if (generatedKey === key) {
                     console.log("NOPE");
                 } else {
-                    console.log("its a new key! ", generatedKey)
+                    console.log("its a new key! ", generatedKey);
                     uniqueKeyGenerated = true;
                 }
-            })
-
+            });
         }
-    }
+    };
 
     const openModal = () => {
         setIsModalOpen(true);
-    }
+    };
     const closeModal = () => {
         setIsModalOpen(false);
     };
-
 
     // Create a course to the database and for others to see, updates asynchronously
     const createCourse = async (e) => {
@@ -128,15 +135,16 @@ function CoursesMain() {
                 displayName = auth.currentUser.email;
             }
             generateCourseKey();
-            await coursesRef.add({
-                courseName: formCourseName,
-                courseType: formCourseType,
-                createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-                uid,
-                photoURL,
-                creatorName: displayName,
-                courseKey: generatedKey
-            })
+            await coursesRef
+                .add({
+                    courseName: formCourseName,
+                    courseType: formCourseType,
+                    createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+                    uid,
+                    photoURL,
+                    creatorName: displayName,
+                    courseKey: generatedKey,
+                })
                 .then((docRef) => {
                     console.log("Document ID: ", docRef.id);
                     joinCourse(generatedKey);
@@ -147,26 +155,26 @@ function CoursesMain() {
 
             setFormCourseName("");
         } else {
-            console.log("Course not created, add valid course name!")
+            console.log("Course not created, add valid course name!");
         }
-    }
+    };
 
     // This can be called whenever needed to join a new course, is automatically called when creating a new course
     const joinCourse = async (newCourseKey) => {
         try {
             await currentUserRef.update({
-                courseCodes: firebase.firestore.FieldValue.arrayUnion(newCourseKey),
+                courseCodes:
+                    firebase.firestore.FieldValue.arrayUnion(newCourseKey),
             });
             console.log("joined course " + newCourseKey);
             refreshCourses();
         } catch (e) {
             console.error("Error joining the course ", e);
         }
-    }
+    };
 
     useEffect(() => {
         refreshCourses();
-
     }, []);
 
     const handleJoinClick = (param) => {
@@ -174,7 +182,7 @@ function CoursesMain() {
             console.log("testi");
             joinCourse(param);
         }
-    }
+    };
 
     return (
         <>
@@ -182,17 +190,22 @@ function CoursesMain() {
                 <div id="btn_cont" className="flex justify-between">
                     <div className="flex">
                         <div id="create_btn" className="m-2">
-                            <button onClick={openModal} className="btn-open-modalbg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <button
+                                onClick={openModal}
+                                className="btn-open-modalbg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                            >
                                 Create Event
                             </button>
                         </div>
                     </div>
                     <div id="join_btn" className="m-2 flex items-start">
-                        <input type="text"
+                        <input
+                            type="text"
                             value={courseKeyText}
                             onChange={(e) => setCourseKeyText(e.target.value)}
                             placeholder="Enter Event Key"
-                            className="mr-2 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                            className="mr-2 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        />
                         <button
                             onClick={() => handleJoinClick(courseKeyText)}
                             className="ml-2 btn-open-modal bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -212,17 +225,30 @@ function CoursesMain() {
                     setFormCourseType={setFormCourseType}
                 />
                 <div className="grid grid-rows-1 gap-4">
-                    {courses && courses.map(msg =>
-                        <Course key={msg.id} message={msg} id={msg.docId} courseKey={msg.courseKey} />
-                    )}
+                    {courses &&
+                        courses.map((msg) => (
+                            <Course
+                                key={msg.id}
+                                message={msg}
+                                id={msg.docId}
+                                courseKey={msg.courseKey}
+                            />
+                        ))}
                 </div>
             </div>
         </>
-    )
+    );
 }
 
-function CourseModal({ isOpen, onRequestClose, createCourse, formCourseName, setFormCourseName, formCourseType, setFormCourseType }) {
-
+function CourseModal({
+    isOpen,
+    onRequestClose,
+    createCourse,
+    formCourseName,
+    setFormCourseName,
+    formCourseType,
+    setFormCourseType,
+}) {
     return (
         <Modal
             isOpen={isOpen}
@@ -230,58 +256,88 @@ function CourseModal({ isOpen, onRequestClose, createCourse, formCourseName, set
             className="modal"
             overlayClassName="overlay"
         >
-            <div id="defaultModal" className="fixed overflow-y-auto overflow-x-hidden outline-none bg-gray-700 rounded-lg shadow dark:bg-gray-700 min-w-[300px]">
+            <div
+                id="defaultModal"
+                className="fixed overflow-y-auto overflow-x-hidden outline-none bg-gray-700 rounded-lg shadow dark:bg-gray-700 min-w-[300px]"
+            >
                 <div className="relative w-auto p-4 border-b rounded-t dark:border-gray-600">
                     <div className="flex items-center justify-center mb-2">
-                        <h2 className="text-2xl font-semibold text-white mb-5">Create Event</h2>
-                        <button type="button"
+                        <h2 className="text-2xl font-semibold text-white mb-5">
+                            Create Event
+                        </h2>
+                        <button
+                            type="button"
                             onClick={onRequestClose}
                             className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                            data-modal-hide="authentication-modal">
-                            <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                viewBox="0 0 14 14">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                            data-modal-hide="authentication-modal"
+                        >
+                            <svg
+                                className="w-3 h-3"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 14 14"
+                            >
+                                <path
+                                    stroke="currentColor"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                                />
                             </svg>
                             <span className="sr-only">Close modal</span>
                         </button>
                     </div>
-                    <form onSubmit={createCourse} className="mb-5 grid grid-cols-1 md:grid-cols-2 gap-4 items-center">
+                    <form
+                        onSubmit={createCourse}
+                        className="mb-5 grid grid-cols-1 md:grid-cols-2 gap-4 items-center"
+                    >
                         {/* Left Section */}
                         <div className="col-span-1 m-2">
                             <div className="p-4 bg-gray-800 text-white m-2 w-[150px] h-[150px]">
                                 <p>Photo goes here</p>
                             </div>
 
-                            <button
-                                className="bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 m-2"
-                            >
+                            <button className="bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 m-2">
                                 Upload photo
                             </button>
-
                         </div>
 
                         {/* Right Section */}
                         <div className="grid grid-cols-4 col-span-1 mr-5">
-                            <label className="block m-2 text-sm font-medium text-gray-900 dark:text-white">Name: </label>
+                            <label className="block m-2 text-sm font-medium text-gray-900 dark:text-white">
+                                Name:{" "}
+                            </label>
                             <input
                                 value={formCourseName}
                                 placeholder="Enter a name, min. 5 characters"
-                                onChange={(e) => setFormCourseName(e.target.value)}
+                                onChange={(e) =>
+                                    setFormCourseName(e.target.value)
+                                }
                                 className="w-full col-span-3 mb-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             />
-                            <label htmlFor="event-types" className="block m-2 text-sm font-medium text-gray-900 dark:text-white">
+                            <label
+                                htmlFor="event-types"
+                                className="block m-2 text-sm font-medium text-gray-900 dark:text-white"
+                            >
                                 Type:
                             </label>
                             <select
                                 value={formCourseType}
-                                onChange={(e) => setFormCourseType(e.target.value)}
+                                onChange={(e) =>
+                                    setFormCourseType(e.target.value)
+                                }
                                 id="event-types"
                                 className="mb-5 col-span-3 w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             >
                                 <option selected>Choose event type</option>
-                                <option value="Course" id="option1">Course</option>
-                                <option value="Event" id="option2">Event</option>
+                                <option value="Course" id="option1">
+                                    Course
+                                </option>
+                                <option value="Event" id="option2">
+                                    Event
+                                </option>
                             </select>
                             <div className="col-span-4 flex items-center justify-center">
                                 <button
