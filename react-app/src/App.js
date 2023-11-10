a51a827fdc725ebf5bf7447a4eb2a17fd81252dd
edvni <edvard.nivala@metropolia.fi>
2023-11-10 20:41:52 +0200

    RTL / LTR implementation
    Right to Left and Left to Right implementation

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import './App.css';
 import { useAuthState } from "react-firebase-hooks/auth"
 import { auth } from "./firebaseConfig"; // Import Firebase config
@@ -8,10 +8,16 @@ import HeaderProfile from "./components/Layout/HeaderProfile";
 import LandingPage from "./components/Pages/LandingPage";
 import Course from "./components/Chat/Course";
 import Contact from "./components/Pages/Contact";
+import {useTranslation} from "react-i18next";
 
 function App() {
 
   const [user] = useAuthState(auth);
+  const { i18n } = useTranslation();
+
+  useEffect(() => {
+      document.dir = i18n.dir();
+  }, [i18n, i18n.language]);
 
   return (
       <Router>
