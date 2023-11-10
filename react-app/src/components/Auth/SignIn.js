23ab3a70650a266e7e451c8ce9a782fc652a1a07
edvni <edvard.nivala@metropolia.fi>
2023-11-10 20:42:22 +0200

    Put new translations in to place

--- react-app/src/components/Auth/SignIn.js
+++ react-app/src/components/Auth/SignIn.js
@@ -3,6 +3,7 @@ import React from "react";
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import googleLogo from '../../images/google_logo.png';
+import {useTranslation} from "react-i18next";
 
 
 // Sign in function, uses Firebase Auth, is only visible when not signed in
@@ -16,11 +17,13 @@ function SignIn() {
         });
     }
 
+    const { t } = useTranslation();
+
     return (
         <div className="mt-8 flex justify-center text-lg text-black">
             <button onClick={signInWithGoogle}
                 className="flex items-center rounded-3xl bg-orange-500 bg-opacity-50 px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-yellow-600">
-                <span>Sign in with</span>
+                <span>{t("sign_in_with")}</span>
                 <img src={googleLogo} alt="google_logo" className="max-w-[25px] ml-2"/>
             </button>
         </div>
--- react-app/src/components/Auth/SignInForm.js
+++ react-app/src/components/Auth/SignInForm.js
@@ -1,8 +1,12 @@
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import {useState} from "react";
 import PasswordAlert from "../Alerts/PasswordAlert";
+import LanguageSwitcher from "../Layout/LanguageSwitcher";
+import {useTranslation} from "react-i18next";
 
 function SignInForm({ onSignUpClick }) {
+
+    const { t } = useTranslation();
 
     const SignIn = (e) =>{
         e.preventDefault();
@@ -38,31 +42,34 @@ function SignInForm({ onSignUpClick }) {
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                 <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                    Sign in to your account
+                    {t("sign_in_header")}
                 </h1>
                 <form onSubmit={SignIn} className="space-y-4 md:space-y-6" action="#">
                     <div>
-                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{t("your_email")}</label>
                         <input type="email" name="email" id="email" value={email} onChange={handleEmailChange}
                                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                placeholder="name@company.com" required=""/>
                     </div>
                     <div>
                         <label htmlFor="password"
-                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{t("password")}</label>
                         <input type="password" name="password" id="password" placeholder="••••••••" value={password} onChange={handlePasswordChange}
                                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                required=""/>
                         {error && <PasswordAlert message={error}/>}
                     </div>
                     <div className="flex items-center justify-between">
-                        <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                        <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">{t("forgot_password")}</a>
                     </div>
-                    <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                    <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">{t("sign_in")}</button>
                     <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                        Don’t have an account yet?
-                        <a href="#" onClick={onSignUpClick} className="font-medium text-primary-600 hover:underline dark:text-primary-500"> Sign up</a>
+                        {t("no_account_yet")}
+                        <a href="#" onClick={onSignUpClick} className="font-medium text-primary-600 hover:underline dark:text-primary-500"> {t("sign_up")}</a>
                     </p>
+                    <div className="text-black w-1/3">
+                        <LanguageSwitcher />
+                    </div>
                 </form>
             </div>
         </div>
