53a3ecf6410cfaaa824c4c49ff4fe921fa8f5de2
Vandarks <majewski.miiko@gmail.com>
2023-11-25 19:52:25 +0200

    Added updateLanguage function that sends users language preference to database, under the users database entry. updateLanguage function combines the database function with the language switch function

--- react-app/src/components/Layout/LanguageSwitcher.js
+++ react-app/src/components/Layout/LanguageSwitcher.js
@@ -1,10 +1,13 @@
 import React from "react";
 import {useTranslation} from "react-i18next";
+import {auth, usersRef} from "../../firebaseConfig";
 
 function LanguageSwitcher() {
 
     const {i18n} = useTranslation();
     const { t } = useTranslation();
+
+    const currentUserRef = usersRef.doc(auth.currentUser.uid);
 
     return (
 
@@ -13,7 +16,7 @@ function LanguageSwitcher() {
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 value={i18n.language}
                 onChange={(e) =>
-                    i18n.changeLanguage(e.target.value)
+                    updateLanguage(e)
                 }
             >
                 <option selected>{t("select_tag_language")}</option>
@@ -23,6 +26,19 @@ function LanguageSwitcher() {
             </select>
         </div>
     );
+
+    function updateLanguage(e) {
+        i18n.changeLanguage(e.target.value)
+        currentUserRef.update({
+            languagePreference: e.target.value
+        })
+            .then(function() {
+                console.log("Document successfully updated!");
+            })
+            .catch(function(error) {
+                console.error("Error updating document: ", error);
+            });
+    }
 }
 
 export default LanguageSwitcher;
