1d57151c939f95dfe69a12fb6adb512a8dc8e2ad
edvni <edvard.nivala@metropolia.fi>
2023-11-26 23:34:00 +0200

    Fixed styling

--- react-app/src/components/Layout/HeaderProfile.js
+++ react-app/src/components/Layout/HeaderProfile.js
@@ -33,7 +33,7 @@ function HeaderProfile() {
 
     return(
         <header>
-            <nav className="fixed top-0 w-full z-10 bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-700">
+            <nav className="fixed top-0 w-full z-10 border-gray-200 px-4 lg:px-6 py-2.5 bg-gray-700">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Link to="/" className="flex items-center">
                         <img src={wfLogo} className="mr-3 h-6 sm:h-9"
@@ -47,7 +47,7 @@ function HeaderProfile() {
                             {user.providerData[0].providerId === "password" && (
                                 
                             <button data-testid="settings_button" className="mr-5" onClick={openModal}>
-                                <img src={settingsLogo} alt="settings logo" className="max-w-[30px]"/>
+                                <img src={settingsLogo} alt="settings logo" className="max-w-[30px] hover:opacity-25"/>
                             </button>
                             )}
                         <SettingsModal
@@ -79,11 +79,11 @@ function HeaderProfile() {
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <Link to="/"
-                                      className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-100 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-800">{t("nav_home")}</Link>
+                                      className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:text-orange-400 lg:border-0 lg:p-0 text-gray-100 dark:border-gray-800">{t("nav_home")}</Link>
                             </li>
                             <li>
                                 <Link to="/contact"
-                                      className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-100 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-800">{t("nav_contact")}</Link>
+                                      className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:text-orange-400 lg:border-0 lg:p-0 text-gray-100 dark:border-gray-800">{t("nav_contact")}</Link>
                             </li>
                         </ul>
                     </div>
